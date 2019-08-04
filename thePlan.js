4bab6557b98efd0ee1935c457dc1b82d181c00ed
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2019-08-03 22:14:26 -0700

    centered <'s &  >'s, includes plan

---
+++ thePlan.js
@@ -0,0 +1,18 @@
+/*The Plan
+
+need to watch video for explanation of the project
+
+I think that I have basic layout done.
+
+I think that I will need to process data into 3 different arrays.
+
+use each array to fill each of the the 'holder' div's divs
+figure out how names move according to slot position using const slots
+
+going to need eventlisteners for < < > >
+
+going to need event listeners for divs that hold names
+
+priority is business logic
+
+*/
