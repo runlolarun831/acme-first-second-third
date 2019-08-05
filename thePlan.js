2c62a2a93f3b1477e7036220f4d7f5846db33056
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2019-08-04 21:51:13 -0700

    css and html

--- thePlan.js
+++ thePlan.js
@@ -3,6 +3,7 @@
 need to watch video for explanation of the project
 
 I think that I have basic layout done.
+I think that I will need to do layout with flex.
 
 I think that I will need to process data into 3 different arrays.
 
@@ -14,5 +15,13 @@ going to need eventlisteners for < < > >
 going to need event listeners for divs that hold names
 
 priority is business logic
+--------------------------------------------------
+name your repo acme-first-second-third
+create an index.html
+you can eventually move your javascript and css to separate files
+focus on seeing results with every code change
+start by displaying the data
+then you can toggle a user being selected when being clicked on
+then you can focus on moving users between lists
 
 */
