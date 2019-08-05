5752e37ba2491b93311b04224b6808c335999dc1
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2019-08-04 21:59:12 -0700

    changed my plan

--- thePlan.js
+++ thePlan.js
@@ -1,16 +1,15 @@
 /*The Plan
 
-need to watch video for explanation of the project
+*need to watch video for explanation of the project
 
 I think that I have basic layout done.
 I think that I will need to do layout with flex.
 
-I think that I will need to process data into 3 different arrays.
-
-use each array to fill each of the the 'holder' div's divs
 figure out how names move according to slot position using const slots
+don't change actual data structure just modify what's in it
+focus on divs
 
-going to need eventlisteners for < < > >
+going to need eventlisteners for < < > > //made those buttons or dead divs
 
 going to need event listeners for divs that hold names
 
